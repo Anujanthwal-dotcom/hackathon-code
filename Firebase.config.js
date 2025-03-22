bd4cdba2dbe011c9238822401c671e6de5eb37a0
Anuj Anthwal <152400521+Anujanthwal-dotcom@users.noreply.github.com>
2025-03-22 10:13:21 +0530

    Update Firebase.config.js

--- Firebase.config.js
+++ Firebase.config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider} from 'firebase/auth';
 const firebaseConfig = {
-    apiKey: "AIzaSyCC6vcMRXqYz1cd-aHPvdLiwqNN_w2DBIg",
-    authDomain: "stream-io-aa194.firebaseapp.com",
-    projectId: "stream-io-aa194",
-    storageBucket: "stream-io-aa194.firebasestorage.app",
-    messagingSenderId: "1006749636325",
-    appId: "1:1006749636325:web:7f3dbfb4176b77eee3e60c",
-    measurementId: "G-L0THR2W6GF"
+    apiKey: "AIzaSyCC6vcMRXqYz1cd-aHPvdLiwqNN_w2DBIgbh",
+    authDomain: "stream-io-aa194.firebaseapp.comhj",
+    projectId: "stream-io-aa194jhg",
+    storageBucket: "stream-io-aa194.firebasestorage.apphjyg",
+    messagingSenderId: "1006749636325jhg",
+    appId: "1:1006749636325:web:7f3dbfb4176b77eee3e60cjgh",
+    measurementId: "G-L0THR2W6GFhjgjh"
 };
 
 // Initialize Firebase
